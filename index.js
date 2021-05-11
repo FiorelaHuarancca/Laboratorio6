2cfe7eedca67e868e35208bbcdc41f34f52bfc9a
Jeydi Fiorela Huarancca Pacheco <jeydi.huarancca@tecsup.edu.pe>
2021-05-11 10:31:31 -0500

    primercommitTEO

--- index.js
+++ index.js
@@ -1,122 +1,121 @@
-import http from 'http'
+/* eslint-disable eqeqeq */
+/* eslint-disable no-undef */
 import express from 'express'
 
-const express = require('express')
+// const express = require('express')
 
-const app = express ()
+const app = express()
 app.use(express.json())
 
 let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-    {
-        id: 5,
-        name: "Fiorela Huarancca",
-        number: "27-21-84235490"
-    }
+
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '39-23-6423122'
+  },
+  {
+    id: 5,
+    name: 'Fiorela Huarancca',
+    number: '27-21-84235490'
+  }
 
 ]
 
+app.use((request, res, next) => {
+  console.log('estoy en el primer middleware')
+  console.log(req.path)
+  console.log(req.method)
+  console.log(req.body)
+})
 
 app.get('/info', (request, response) => {
-    let fechaHora = new Date()
-    let mensaje = `<p>Phonebook has info for ${persons.length} people</p>
+  const fechaHora = new Date()
+  const mensaje = `<p>Phonebook has info for ${persons.length} people</p>
                  <p>${fechaHora}</p>\``
-    response.send(mensaje)
+  response.send(mensaje)
 })
-
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id == id)
+  const id = request.params.id
+  const person = persons.find(person => person.id == id)
 
-    console.log(person)
+  console.log(person)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
-
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
-})
-
-
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.status(204).end()
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(notes)
+  response.json(persons)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
 })
 
 app.post('/api/persons', (request, response) => {
+  const body = request.body
+  let error
+  console.log(body)
 
-    const body = request.body
-    let error ;
-    console.log(body)
+  if (!body.name) {
+    error = 'Falta el nombre'
+  }
 
-    if (!body.name) {
-        error = "Falta el nombre"
-    }
+  if (!body.number) {
+    error = 'Falta el número'
+  }
 
-    if (!body.number) {
-        error = "Falta el número"
-    }
+  if (!body.name && !body.number) {
+    error = 'No se encontro el nombre ni la agenda'
+  }
 
-    if (!body.name && !body.number) {
-        error = "No se encontro el nombre ni la agenda"
-    }
+  if (error) {
+    return response.status(400).json({
+      error: error
+    })
+  }
 
-    if (error) {
-        return response.status(400).json({
-            error: error
-        })
-    }
+  const person = {
+    id: getRandomInt(100, 10000),
+    name: request.body.name,
+    number: request.body.number
+  }
 
- 
-    const person = {
-        id:getRandomInt(100,10000),
-        name:request.body.name,
-        number:request.body.number,
-    }
-
-    response.json(person)
+  response.json(person)
 })
 
-function getRandomInt(minimo, maximo) {
-    minimo = Math.ceil(minimo);
-    maximo = Math.floor(maximo);
-    return Math.floor(Math.random() * (maximo - minimo) + minimo); 
+function getRandomInt (minimo, maximo) {
+  minimo = Math.ceil(minimo)
+  maximo = Math.floor(maximo)
+  return Math.floor(Math.random() * (maximo - minimo) + minimo)
 }
-
-
 
 const PORT = 3001
 app.listen(PORT)
